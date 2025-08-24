e4961a6f4a304d826182a014a0e8d68760d442ff
YukiTsuneyoshi <momommo808080@gmail.com>
2025-08-25 02:38:53 +0900

    階層に応じてRingマークの移動

---
+++ Assets/Scripts/UI/UIRing.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class UIRing : MonoBehaviour
+{
+    [Tooltip("ヒエラルキー上のAmountMap"), SerializeField] private GameObject amountMap;
+    float mapX, mapY;
+    float offset = -80f;
+    float space = 60;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetUIPosition();
+    }
+
+    void SetUIPosition()
+    {
+        //GameManager.nowStageによってRingの位置を変える
+        //ステージは0～2まで
+        mapX = amountMap.GetComponent<RectTransform>().localPosition.x;
+        mapY = amountMap.GetComponent<RectTransform>().localPosition.y + offset + space * GameManager.nowStage;
+        GetComponent<RectTransform>().localPosition = new Vector3(mapX, mapY, 0);
+    }
+}
