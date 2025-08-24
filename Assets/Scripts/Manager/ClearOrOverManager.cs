3d00a6cf69621ecd03753655ca0d54f0d152a09a
YukiTsuneyoshi <momommo808080@gmail.com>
2025-08-24 22:25:45 +0900

    テストシーン追加、ゲームクリア時に敵の動きを止める

--- Assets/Scripts/Manager/ClearOrOverManager.cs
+++ Assets/Scripts/Manager/ClearOrOverManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 using DG.Tweening;
 
 public class ClearOrOverManager : MonoBehaviour
@@ -8,7 +9,7 @@ public class ClearOrOverManager : MonoBehaviour
     [SerializeField] ViewManager viewManager;
     [SerializeField] StageChanger stageChanger;
     [SerializeField] GameObject clearImage;
-    [SerializeField] NavMeshAgentController navMeshAgentController;
+    [SerializeField] NavMeshAgent navMeshAgent;
     private int _stage = 0;
     private int _2Dor3D = 0;
     private bool clearStarted = false;
@@ -75,6 +76,8 @@ public class ClearOrOverManager : MonoBehaviour
     public IEnumerator ClearEffect() {
         //クリア画像表示
         GameManager.isWaiting = true;
+        //敵のsetActiveをfalseに
+        navMeshAgent.gameObject.SetActive(false);
         yield return new WaitForSeconds(1);
         var image = clearImage.GetComponent<Image>();
         image.DOFade(1, 2);
