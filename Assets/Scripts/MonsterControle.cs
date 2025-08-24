0e46109d459e2bbd87b3aa6414663e67e5d292e2
shishiboy3939 <shishiboy3939@gmail.com>
2025-08-25 01:56:51 +0900

    とりあえずKillCamera遷移はできた

--- Assets/Scripts/MonsterControle.cs
+++ Assets/Scripts/MonsterControle.cs
@@ -1,16 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterControle : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] ClearOrOverManager clearOrOverManager;
-
+    [SerializeField] NavMeshAgent agent; 
     void OnTriggerEnter(Collider col)
     {
         //プレイヤーと接触した場合
-        if (col.gameObject.name == player.name)
+        if (col.gameObject == player)
         {
-            clearOrOverManager.GameOver();
+            Debug.Log("衝突は検出できてる");
+            StartCoroutine(ChangeKillCam());
         }
+    }
+    IEnumerator ChangeKillCam()
+    {
+        Debug.Log("ChangeKillCamは呼べている");
+        if (agent != null) agent.isStopped = true;
+        ViewManager.Instance.camera3D.SetActive(false);
+        ViewManager.Instance.killCamera.SetActive(true);
+        GameManager.isWaiting = true;
+        yield return new WaitForSeconds(3);
+        ViewManager.Instance.killCamera.SetActive(false);
+        if (agent != null) agent.isStopped = false;
+        clearOrOverManager.GameOver();
     }
 }
--- Assets/Scripts/StageChanger.cs
+++ Assets/Scripts/StageChanger.cs
@@ -70,17 +70,6 @@ public class StageChanger : MonoBehaviour
             //3Dステージの線をステージ標高に沿った高さに
             //標高を測りたいオブジェクトにはGroundタグを付けて！！！
             viewManager.StrokeManager3D.SetupStrokeHeight(viewManager, stage);
-            //ステージプレハブを全てアクティブに
-            // 親を含む子オブジェクトを再帰的に取得
-            // trueを指定しないと非アクティブなオブジェクトを取得できないことに注意
-            var parentAndChildren = viewManager.Stages[stage].mazeCubes.transform.GetComponentsInChildren<Transform>(true);
-            var children = new Transform[parentAndChildren.Length - 1];
-            // 親を除く子オブジェクトを結果にコピー
-            Array.Copy(parentAndChildren, 1, children, 0, children.Length);
-            foreach (Transform n in children)
-            {
-                n.gameObject.SetActive(true);
-            }
             //プレイヤーを指定座標に配置
             viewManager.playerCapsule.transform.position = viewManager.Stages[stage].playerPosition;
             viewManager.playerCapsule.transform.localEulerAngles = viewManager.Stages[stage].playerRotation;
