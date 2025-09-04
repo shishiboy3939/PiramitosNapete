d6faa753b7bf287c0f1bd61c1b57a642c7af0868
shishiboy <83420988+shishiboy@users.noreply.github.com>
2025-09-04 15:09:10 +0900

    BlackOut元に戻した。これでも一瞬は映るし、マップ画面のSEは鳴る。抜本的な解決が必要

--- Assets/Scripts/Manager/ClearOrOverManager.cs
+++ Assets/Scripts/Manager/ClearOrOverManager.cs
@@ -102,6 +102,7 @@ public class ClearOrOverManager : MonoBehaviour
             {
                 //エンディング再生でフェードインするバグが起きるかも
                 //多分そんなに致命的ではないけど一応TODO
+                image.DOFade(1, 0);
                 VideoManager.Instance.EndingPlay();
                 _stage = 0;
             }
